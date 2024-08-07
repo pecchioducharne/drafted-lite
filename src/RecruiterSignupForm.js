97fc01333fccc01fa8fc9b5b979e9a8e1cefc5dc
pecchioducharne <pecchioducharne@gmail.com>
2024-08-07 18:35:36 -0400

    adjust error

--- src/RecruiterSignupForm.js
+++ src/RecruiterSignupForm.js
@@ -379,6 +379,7 @@ const RecruiterSignupForm = () => {
                   "Complete Signup"
                 )}
               </button>
+              <br></br>
               {signupError && (
                 <p style={{ color: "red", marginBottom: "10px" }}>
                   {signupError}
